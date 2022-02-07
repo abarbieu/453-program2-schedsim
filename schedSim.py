3956b1225c6fdb4e221f840f0dd2bf106a9c84f8
Aidan Barbieux <abarbieu@calpoly.edu>
2022-02-07 13:56:39 -0800

    yoga, fifo and turnaround done

--- schedSim.py
+++ schedSim.py
@@ -3,9 +3,25 @@ import numpy as np
 import argparse
 
 def readAssign(file):
-    f = open(file)
-    for l in f.readlines():
-        print(np.array(l.strip('\n').split(' '),dtype=int))
+    f = open(file, mode='r')
+    lines = np.array([l.strip('\n').split(' ') for l in f.readlines()], dtype=float)
+    f.close()
+    return lines[lines[:,1].argsort()]
+
+def printTurnaround(jobs, wait):
+    for i in range(len(jobs)):
+        print(f"Job {i:3d} -- Turnaround {wait[i] + jobs[i,0]:3.2f} Wait {wait[i]:3.2f}")
+
+def fifo(jobs):
+    wait = [0]*jobs.shape[0]
+    i = 0
+    time = 0
+    while i < len(jobs):
+        time = max(time,jobs[i,1])
+        wait[i] += time - jobs[i,1]
+        time += jobs[i,0]
+        i += 1
+    return wait
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Deal with jobs.txt -q and -p')
@@ -26,4 +42,8 @@ if __name__ == "__main__":
     p = args.p
     q = args.q
 
-    readAssign(jobfile)
+    jobs = readAssign(jobfile)
+    wait=fifo(jobs)
+
+    printTurnaround(jobs,wait)
+    
